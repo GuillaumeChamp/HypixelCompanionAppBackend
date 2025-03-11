b168fffa672b34016d4781faf52fcc498ee867c7
guillaume <guillaume.champtoussel@ensea.fr>
2025-03-11 11:24:45 +0100

    minor code improvement

--- src/main/java/com/example/hypixeltrackerbackend/repository/ItemPricingRepository.java
+++ src/main/java/com/example/hypixeltrackerbackend/repository/ItemPricingRepository.java
@@ -1,14 +1,16 @@
 package com.example.hypixeltrackerbackend.repository;
 
 import com.example.hypixeltrackerbackend.data.bazaar.ItemPricing;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface ItemPricingRepository extends CrudRepository<ItemPricing, Long>{
+@Repository
+public interface ItemPricingRepository extends JpaRepository<ItemPricing, Long> {
 
     List<ItemPricing> findAllByItemIdAndLastUpdateBetweenOrderByLastUpdate(String itemId, LocalDateTime lastUpdate, LocalDateTime lastUpdate2);
     @Query("""
@@ -19,7 +21,8 @@ public interface ItemPricingRepository extends CrudRepository<ItemPricing, Long>
     List<ItemPricing> groupAllByTimestampBetween(@Param("before") LocalDateTime before,
                                                  @Param("after") LocalDateTime after);
 
-    void deleteAllByLastUpdateBetween(LocalDateTime before, LocalDateTime after);
+    void deleteAllInBatchByLastUpdateBetween(LocalDateTime before, LocalDateTime after);
 
     List<ItemPricing> findAllByItemId(String itemId);
+
 }
--- src/main/java/com/example/hypixeltrackerbackend/services/DataProcessorService.java
+++ src/main/java/com/example/hypixeltrackerbackend/services/DataProcessorService.java
@@ -2,12 +2,11 @@ package com.example.hypixeltrackerbackend.services;
 
 import com.example.hypixeltrackerbackend.constant.TimeConstant;
 import com.example.hypixeltrackerbackend.data.bazaar.CompleteItem;
-import com.example.hypixeltrackerbackend.data.bazaar.Craft;
 import com.example.hypixeltrackerbackend.data.bazaar.ItemPricing;
 import com.example.hypixeltrackerbackend.data.mapper.ItemPricingMapper;
 import com.example.hypixeltrackerbackend.data.mapper.StaticItemMapper;
 import com.example.hypixeltrackerbackend.repository.ItemPricingRepository;
-import com.example.hypixeltrackerbackend.utils.CollectionsUtils;
+import com.example.hypixeltrackerbackend.utils.ItemPricingUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -103,7 +102,7 @@ public class DataProcessorService {
 
     private void groupRecordsWithTimeStampAndWindowSize(LocalDateTime begin, Integer samplingTimeWindow) {
         List<ItemPricing> summary = pricingRepository.groupAllByTimestampBetween(begin, begin.plusMinutes(samplingTimeWindow));
-        pricingRepository.deleteAllByLastUpdateBetween(begin, begin.plusMinutes(samplingTimeWindow));
+        pricingRepository.deleteAllInBatchByLastUpdateBetween(begin, begin.plusMinutes(samplingTimeWindow));
         pricingRepository.saveAll(summary);
     }
 
@@ -124,138 +123,7 @@ public class DataProcessorService {
         });
         Collection<ItemPricing> prices = bazaarItemList.values();
         pricingRepository.saveAll(prices);
-        completeItemHashMap.forEach((key, value) -> computeItemMinimalCost(value));
-    }
-
-    /**
-     * Resolve minimal cost value using craft if available or else buy price.
-     * NB if an item have no craft and no bazaar order, the minimal cost will remain null.
-     *
-     * @param item a completeItem to resolve, will be call recursively if needed
-     */
-    private void computeItemMinimalCost(CompleteItem item) {
-        if (item.getName() == null) {
-            logger.log(Level.WARNING, "item without name : {0} please check if item database is up to date", item);
-            return;
-        }
-        if (CollectionsUtils.isEmpty(item.getCrafts())) {
-            if (item.getPricing() == null) {
-                logger.fine("no pricing for " + item.getName());
-                return;
-            }
-            if (item.getPricing().getBuyPrice() == null) {
-                logger.fine("no buy price" + item.getName() + " might not have sell order at this moment");
-                return;
-            }
-            item.getPricing().setMinimalPrice(item.getPricing().getBuyPrice());
-            return;
-        }
-        for (Craft craft : item.getCrafts()) {
-            try {
-                updateMinimalCostUsingOneCraft(item, craft);
-            } catch (DataProcessingException e) {
-                logger.log(Level.INFO, e::getMessage);
-            }
-        }
-    }
-
-    /**
-     * Update the minimal cost by resolving the crafting cost using the given craft
-     *
-     * @param item  the item to update
-     * @param craft one craft of the item to check
-     * @throws DataProcessingException if unable to compute craft cost
-     */
-    private void updateMinimalCostUsingOneCraft(CompleteItem item, Craft craft) throws DataProcessingException {
-        if (craft.getCraftingCost() == null) {
-            computeCraftCost(craft);
-        }
-        // check pricing after because craft cost can be useful even if there is no pricing
-        if (item.getPricing() == null) {
-            logger.fine(item.getName() + " : has no pricing");
-            return;
-        }
-        updateMinimalPrice(item.getPricing(), craft.getCraftingCost());
-    }
-
-    /**
-     * Update the minimal price value by setting it to the default (i.e. the buy cost)
-     *
-     * @param pricing      the pricing sheet of an item
-     * @param craftingCost the cost to check with
-     */
-    private void updateMinimalPrice(ItemPricing pricing, Double craftingCost) {
-        Double buyPrice = pricing.getBuyPrice();
-        // by the provider api, the buyPrice is 0 if item is not currently sell
-        if (buyPrice == null || buyPrice > craftingCost) {
-            pricing.setMinimalPrice(craftingCost);
-        } else {
-            pricing.setMinimalPrice(buyPrice);
-        }
-    }
-
-    /**
-     * Compute the crafting cost of an item
-     *
-     * @param craft the craft to complete
-     * @throws DataProcessingException is thrown if it cannot compute cost of a material
-     */
-    private void computeCraftCost(Craft craft) throws DataProcessingException {
-        double craftingCost = 0;
-        List<String> materials = craft.getMaterialIdList();
-        List<Float> quantities = craft.getQuantityOfMaterial();
-        for (int i = 0; i < materials.size(); i++) {
-            String materialId = materials.get(i);
-            float quantity = quantities.get(i);
-            Double extractedCost = extractCostForAMaterial(materialId);
-            if (extractedCost == null) {
-                throw new DataProcessingException("Cannot extract cost of : " + materialId);
-            }
-            craftingCost += extractedCost * quantity;
-        }
-        craft.setCraftingCost(craftingCost);
-    }
-
-    /**
-     * Extract the cost of a material
-     *
-     * @param materialId id of a material to find
-     * @return the extracted value
-     * @throws DataProcessingException is thrown when a material is not found or if an item have no minimal price even after a call to compute item minimal price
-     */
-    private Double extractCostForAMaterial(String materialId) throws DataProcessingException {
-        if (!completeItemHashMap.containsKey(materialId)) {
-            throw new DataProcessingException(materialId.concat(" : is missing from local database"));
-        }
-        CompleteItem completeItem = completeItemHashMap.get(materialId);
-        ItemPricing itemPricing = completeItem.getPricing();
-        double minimalPrice;
-        if (itemPricing == null) {
-            minimalPrice = handleMissingPricing(completeItem);
-        } else {
-            if (itemPricing.getMinimalPrice() == null) {
-                computeItemMinimalCost(completeItem);
-            }
-            if (itemPricing.getMinimalPrice() == null) {
-                throw new DataProcessingException("unable to compute minimal cost", completeItem);
-            }
-            minimalPrice = itemPricing.getMinimalPrice();
-        }
-        return minimalPrice;
-    }
-
-    private double handleMissingPricing(CompleteItem completeItem) throws DataProcessingException {
-        if (CollectionsUtils.isNotEmpty(completeItem.getCrafts())) {
-            if (completeItem.getCrafts().get(0).getCraftingCost() == null) {
-                // old school for loop to handle exception
-                for (Craft craft : completeItem.getCrafts()) {
-                    computeCraftCost(craft);
-                }
-            }
-            return completeItem.getCrafts().get(0).getCraftingCost();
-        } else {
-            throw new DataProcessingException("no pricing and no craft, craft aborted", completeItem);
-        }
+        ItemPricingUtil.updateAllItemsMinimalCost(completeItemHashMap);
     }
 
 }
